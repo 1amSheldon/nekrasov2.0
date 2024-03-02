886b81dd4e6f7ed5ff70f4e7eaa75eff427cc00c
1amSheldon <v.i.zubarev@bk.ru>
2024-03-02 17:24:50 +0700

    some fix in KB

--- keyboards/kb_model.py
+++ keyboards/kb_model.py
@@ -7,25 +7,25 @@ async def modelMenuKb(id):
     model = databaseUsers.getModel(id)
     if model == 0:
         buttons = [[InlineKeyboardButton(text="✅gpt-3.5-turbo", callback_data="changeGPT3.5")],
-                   [InlineKeyboardButton(text="gpt-4", callback_data="changeGPT4")],
+                   [InlineKeyboardButton(text="gpt-4(не стабильна)", callback_data="changeGPT4")],
                    [InlineKeyboardButton(text="gpt-4-turbo", callback_data="changeGPT4Turbo")],
                    [InlineKeyboardButton(text="gpt-4-vision", callback_data="changeGPTVision")],
                    [InlineKeyboardButton(text="←Назад", callback_data="gptExit")]]
     elif model == 2:
         buttons = [[InlineKeyboardButton(text="gpt-3.5-turbo", callback_data="changeGPT3.5")],
-                   [InlineKeyboardButton(text="gpt-4", callback_data="changeGPT4")],
+                   [InlineKeyboardButton(text="gpt-4(не стабильна)", callback_data="changeGPT4")],
                    [InlineKeyboardButton(text="✅gpt-4-turbo", callback_data="changeGPT4Turbo")],
                    [InlineKeyboardButton(text="gpt-4-vision", callback_data="changeGPTVision")],
                    [InlineKeyboardButton(text="←Назад", callback_data="gptExit")]]
     elif model == 1:
         buttons = [[InlineKeyboardButton(text="gpt-3.5-turbo", callback_data="changeGPT3.5")],
-                   [InlineKeyboardButton(text="✅gpt-4", callback_data="changeGPT4")],
+                   [InlineKeyboardButton(text="✅gpt-4(не стабильна)", callback_data="changeGPT4")],
                    [InlineKeyboardButton(text="gpt-4-turbo", callback_data="changeGPT4Turbo")],
                    [InlineKeyboardButton(text="gpt-4-vision", callback_data="changeGPTVision")],
                    [InlineKeyboardButton(text="←Назад", callback_data="gptExit")]]
     else:
         buttons = [[InlineKeyboardButton(text="gpt-3.5-turbo", callback_data="changeGPT3.5")],
-                   [InlineKeyboardButton(text="gpt-4", callback_data="changeGPT4")],
+                   [InlineKeyboardButton(text="gpt-4(не стабильна)", callback_data="changeGPT4")],
                    [InlineKeyboardButton(text="gpt-4-turbo", callback_data="changeGPT4Turbo")],
                    [InlineKeyboardButton(text="✅gpt-4-vision", callback_data="changeGPTVision")],
                    [InlineKeyboardButton(text="←Назад", callback_data="gptExit")]]
